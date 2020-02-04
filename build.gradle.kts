15e58e290fe533b3b6e5ceec015c02099ada4b1b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-02-04 07:54:47 +0100

    Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = ext.get("ktorVersion").toString()
-val dusseldorfKtorVersion = "1.2.3.ec226d3"
+val dusseldorfKtorVersion = "1.3.0.b7013ab"
 val pdfBoxVersion = "2.0.16"
 val mainClass = "no.nav.helse.K9JoarkKt"
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
@@ -31,18 +31,29 @@ dependencies {
     compile("org.apache.pdfbox:pdfbox:$pdfBoxVersion")
 
     // Test
-    testCompile ( "no.nav.helse:dusseldorf-ktor-test-support:$dusseldorfKtorVersion")
-
-    testCompile("io.ktor:ktor-server-test-host:$ktorVersion") {
+    testCompile("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testCompile ("io.ktor:ktor-server-test-host:1.3.0") {
         exclude(group = "org.eclipse.jetty")
     }
     testCompile("org.skyscreamer:jsonassert:1.5.0")
+    implementation(kotlin("stdlib-jdk8"))
+
+    testCompile ("org.skyscreamer:jsonassert:1.5.0")
 }
 
 repositories {
     maven("https://dl.bintray.com/kotlin/ktor")
     maven("https://kotlin.bintray.com/kotlinx")
     maven("http://packages.confluent.io/maven/")
+
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/navikt/dusseldorf-ktor")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 
     jcenter()
     mavenLocal()
@@ -73,5 +84,5 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "5.5.1"
+    gradleVersion = "5.6.2"
 }
