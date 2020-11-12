c234c37e450cd9696845dab8c60dc61c17024399
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-11-12 14:38:45 +0100

    Lager nytt endepunkt for dele-dager og egen tittel
    Signed-off-by: Sander Opperud Odden <sander.opperud.odden@nav.no>

--- src/main/kotlin/no/nav/helse/journalforing/v1/BrevkodeTittelOgTema.kt
+++ src/main/kotlin/no/nav/helse/journalforing/v1/BrevkodeTittelOgTema.kt
@@ -37,6 +37,12 @@ internal object BrevkodeTittelOgTema {
         Kapittel9Ytelse
     )
 
+    private val OMSORGSPENGER_DELING_AV_DAGER = Triple(
+        BrevKode(brevKode = "NAV 09-06.08", dokumentKategori = "SOK"),
+        "Melding om deling av omsorgsdager - NAV 09-06.08",
+        Kapittel9Ytelse
+    )
+
     private val OMSORGSPENGER_MIDLERTIDIG_ALENE = Triple(
         BrevKode(brevKode = "NAV 09-06.07", dokumentKategori = "SOK"),
         "Søknad om å bli regnet som alene  - NAV 09-06.07",
@@ -61,6 +67,7 @@ internal object BrevkodeTittelOgTema {
         Søknadstype.OMSORGSPENGESØKNAD_UTBETALING_FRILANSER_SELVSTENDIG -> OMSORGSPENGER_UTBETALING_FRILANSER_OG_SELVSTENDIG
         Søknadstype.OMSORGSPENGESØKNAD_UTBETALING_ARBEIDSTAKER -> OMSORGSPENGER_UTBETALING_ARBEIDSTAKER
         Søknadstype.OMSORGSPENGESØKNAD_OVERFØRING_AV_DAGER -> OMSORGSPENGER_OVERFØRING_AV_DAGER
+        Søknadstype.OMSORGSPENGEMELDING_DELING_AV_DAGER -> OMSORGSPENGER_DELING_AV_DAGER
         Søknadstype.OMSORGSPENGESØKNAD_MIDLERTIDIG_ALENE -> OMSORGSPENGER_MIDLERTIDIG_ALENE
         Søknadstype.OPPLÆRINGSPENGESØKNAD -> OPPLÆRINGSPENGER
         Søknadstype.FRISINNSØKNAD -> FRISINN
