dcf2ac93b8b405b37b7796c816319d920f48f875
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-01-05 09:49:26 +0100

    Oppdaterer pakker
    * Closes #87
    * Closes #88
    * Closes #89
    * Closes #90
    * Closes #91
    
    Signed-off-by: Sander Opperud Odden <sander.opperud.odden@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = ext.get("ktorVersion").toString()
-val dusseldorfKtorVersion = "3.1.6.7-7d29e37"
-val pdfBoxVersion = "2.0.24"
-val ImageIOVersion = "3.8.0"
+val dusseldorfKtorVersion = "3.1.6.7-1288df6"
+val pdfBoxVersion = "2.0.25"
+val ImageIOVersion = "3.8.1"
 val jsonassertVersion = "1.5.0"
 val fuelVersion = "2.3.1"
 
@@ -12,11 +12,11 @@ val mainClass = "no.nav.helse.K9JoarkKt"
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/109ae9ba66daf97083e807d314a515fd72d6e69c/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/1288df6f88f6fa07ab0007df2a36c10eb7a71402/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -86,7 +86,7 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "7.2"
+    gradleVersion = "7.3.3"
 }
 
 tasks.withType<Test> {
